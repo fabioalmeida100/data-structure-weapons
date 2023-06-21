d5a45c0c5bf93d8a918cebe7a89bcc991dd3ce91
Fabio Almeida <fabioalmeida@fabiodeveloper.com>
2023-06-21 16:15:25 -0300

    Initial commit

---
+++ bynary_search/typescript/bynary_search.ts
@@ -0,0 +1,20 @@
+export default function bynary_search(listItens: number[], item: number): number {
+    let low = 0;
+    let high = listItens.length - 1;
+    let mid = 0;
+
+    while (low <= high) {
+        mid = Math.floor((low + high) / 2);
+
+        if (listItens[mid] === item)
+            return mid;
+        
+        if (listItens[mid] < item)
+            low = mid + 1;
+        
+        if (listItens[mid] > item)
+            high = mid - 1;
+    }
+
+    return -1;
+}
---
+++ index.ts
@@ -0,0 +1,3 @@
+import bynary_search from "./bynary_search/typescript/bynary_search";
+
+console.log(bynary_search([1, 2, 3, 4, 5], 2));
