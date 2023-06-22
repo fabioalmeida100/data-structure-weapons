26601eda3054fd7698f6fe6cd234051558266939
Fabio Almeida <fabioalmeida@fabiodeveloper.com>
2023-06-22 13:03:30 -0300

    Update index in bubble sort

--- src/sort_algorithms/typescript/bubble_sort.ts
+++ src/sort_algorithms/typescript/bubble_sort.ts
@@ -4,7 +4,8 @@ export default function bubbleSort(arr: number[], asc: boolean): number[] {
 
     for (let i = 0; i < arrLength; i++) {
         for (let j = i; j < arrLength; j++) {
-            if ((asc && arr[i] > arr[j]) || (!asc && arr[i] < arr[j])) {
+            let nextElement = arr[j + 1];
+            if ((asc && arr[i] > nextElement) || (!asc && arr[i] < nextElement)) {
                 lowestElement = arr[i];
                 arr[i] = arr[j];
                 arr[j] = lowestElement;
