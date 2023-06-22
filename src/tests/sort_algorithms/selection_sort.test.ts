89c5864e1cd6c7a5f750567114fd9f46929b4a6c
Fabio Almeida <fabioalmeida@fabiodeveloper.com>
2023-06-22 18:57:49 -0300

    Add test for selection sort algorithm

---
+++ src/tests/sort_algorithms/selection_sort.test.ts
@@ -0,0 +1,17 @@
+import selectionSort from "../../sort_algorithms/typescript/selection_sort";
+
+describe("Selection Sort", () => {
+    it("should sort an array of numbers in ascending order", () => {
+        const array = [1, 5, 3, 2, 4];
+        const sortedArr = selectionSort(array, true);
+
+        expect(sortedArr).toEqual([1, 2, 3, 4, 5]);
+    });
+
+    it("should sort an array of numbers in descending order", () => {
+        const array = [1, 5, 3, 2, 4];
+        const sortedArr = selectionSort(array, false);
+
+        expect(sortedArr).toEqual([5, 4, 3, 2, 1]);
+    });
+});
