e950548631dd67336ff93976a32388a3e61d8ffd
Fabio Almeida <fabioalmeida@fabiodeveloper.com>
2023-06-22 19:07:55 -0300

    Add test for recursive search algorithm

--- src/search_algorithms/typescript/recursive_search.ts
+++ src/search_algorithms/typescript/recursive_search.ts
@@ -1,5 +1,7 @@
-export default function recursiveSearch<T>(array: number[], value: number, index: number = 0): number {
-    if (array[index] === value) {
+export default function recursiveSearch(array: number[], value: number, index: number = 0): number {
+    if (index >= array.length) {
+        return -1;
+    } else if (array[index] === value) {
         return index;
     } else
         return recursiveSearch(array, value, index + 1);
---
+++ src/tests/search_algorithms/recursive_search.test.ts
@@ -0,0 +1,17 @@
+import recursiveSearch from "../../search_algorithms/typescript/recursive_search";
+
+describe("Recursive Search", () => {
+    it("should find the index of an element in an array of numbers", () => {
+        const array = [1, 15, 3, 4, 5, 2];
+        const index = recursiveSearch(array, 3);
+
+        expect(index).toBe(2);
+    });
+
+    it("should return -1 if the element is not found in the array", () => {
+        const array = [1, 15, 3, 4, 5, 2];
+        const index = recursiveSearch(array, 6);
+
+        expect(index).toBe(-1);
+    });
+});
