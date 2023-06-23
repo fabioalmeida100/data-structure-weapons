fa6702d88cbc1414c1eeb193b58b0ef066a6fca0
Fabio Almeida <fabioalmeida@fabiodeveloper.com>
2023-06-22 22:17:23 -0300

    Add length of an array using divide and conquer.

---
+++ src/dc/length_array_dc.ts
@@ -0,0 +1,15 @@
+/*
+    Length of an array using divide and conquer.
+    Base case: if array is empty, return 0
+    Recursive case: return 1 + length of array without first element
+*/
+
+export function lengthArrayDC(arr: number[]): number {
+ 
+    if (arr.length === 0) {
+        return 0
+    }
+
+    return 1 + lengthArrayDC(arr.slice(1))
+}
+
